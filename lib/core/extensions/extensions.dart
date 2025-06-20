0cf022f1b8464b9632d2622b02428f68a314a6b9
ali72_20 <as20201118@gmail.com>
2025-06-20 20:33:20 +0300

    add localization support for Arabic and English languages

--- lib/core/extensions/extensions.dart
+++ lib/core/extensions/extensions.dart
@@ -1,2 +1,8 @@
 
 
+import 'package:flutter/cupertino.dart';
+
+extension Context on BuildContext{
+  double get height => MediaQuery.of(this).size.height;
+  double get width => MediaQuery.of(this).size.width;
+}
--- lib/di/di.config.dart
+++ lib/di/di.config.dart
@@ -19,6 +19,8 @@ import '../data/repositories/auth_repository_impl.dart' as _i74;
 import '../domain/repositories/auth_repository.dart' as _i800;
 import '../presentation/ui/login/managers/login_screen_view_model.dart'
     as _i139;
+import '../presentation/ui/login/managers/login_text_controllers_manager.dart'
+    as _i256;
 
 extension GetItInjectableX on _i174.GetIt {
   // initializes the registration of main-scope dependencies inside of GetIt
@@ -29,6 +31,9 @@ extension GetItInjectableX on _i174.GetIt {
     final gh = _i526.GetItHelper(this, environment, environmentFilter);
     final networkModule = _$NetworkModule();
     gh.factory<_i139.LoginScreenViewModel>(() => _i139.LoginScreenViewModel());
+    gh.singleton<_i256.LoginTextControllerManager>(
+      () => _i256.LoginTextControllerManager(),
+    );
     gh.lazySingleton<_i361.Dio>(() => networkModule.dioProvider());
     gh.singleton<_i93.ApiManager>(() => _i93.ApiManager(gh<_i361.Dio>()));
     gh.factory<_i800.AuthRepository>(() => _i74.AuthRepositoryImpl());
