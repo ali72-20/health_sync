a52adcc2b97eee9d302d223d43ec64e89092e0c6
ali72_20 <as20201118@gmail.com>
2025-07-08 00:58:33 +0300

    done

--- lib/presentation/ui/home/widgets/home_body.dart
+++ lib/presentation/ui/home/widgets/home_body.dart
@@ -39,7 +39,7 @@ class _HomeBodyState extends State<HomeBody> {
   List<Widget> views = [
     DashBoardView(),
     DoctorsView(),
-    ClinicDetailsPage(),
+    ClinicsScreen(),
     ReportsView(),
     ProfileView(),
   ];
